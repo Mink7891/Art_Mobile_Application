0a5d96c7d121f6a2e600adbc3a69e9c0bf351718
BUREVOY <mamakolama@gmail.com>
2023-06-06 22:56:44 +0300

    переделал эти ваши видеокурсы
    шоб как в ютубе было

--- pages/VideoCourses.js
+++ pages/VideoCourses.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Image, TouchableOpacity, Text, Linking, ImageBackground} from 'react-native';
+import {StyleSheet, View, Image, TouchableOpacity, Text, Linking, ImageBackground, ScrollView} from 'react-native';
+import styles from './VideoCoursesStyles';
+
 
 const videos = [
   {
@@ -59,14 +61,15 @@ const VideoCourses = () => {
 
   return (
 
-    <View style={styles.container}>
-      <ImageBackground source={require('../assets/pattern22.png')} style={styles.backgroundImage}>
+    <ScrollView style={styles.container}>
+      <ImageBackground source={require('../assets/pattern22.png')} style={styles.backgroundImage} blurRadius={15}>
         <View style={styles.filterContainer}>
-
+        
           <TouchableOpacity
             style={[styles.filterButton, selectedCategory === 'Хореография' && styles.activeFilterButton]}
             onPress={() => handleCategoryFilter('Хореография')}
           >
+            
             <Text
               style={[styles.filterButtonText, selectedCategory === 'Хореография' && styles.activeFilterButtonText]}>
               Хореография
@@ -96,7 +99,7 @@ const VideoCourses = () => {
               Скрипка
             </Text>
           </TouchableOpacity>
-
+          
         </View>
 
         <View style={styles.videoContainer}>
@@ -106,117 +109,28 @@ const VideoCourses = () => {
               style={styles.card}
               onPress={() => handleVideoPress(video.url)}
             >
-              <Image source={video.image} style={styles.image}/>
+              <View style={styles.imageContainer}>
+                <Image source={video.image} style={styles.image}/>
+              </View>
+
               <View style={styles.overlay}>
                 <Text style={styles.title}>{video.title}</Text>
                 <Text style={styles.description}>{video.description}</Text>
               </View>
+
             </TouchableOpacity>
+            
           ))}
         </View>
 
       </ImageBackground>
-    </View>
+    </ScrollView>
 
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F6F6F6',
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
-  filterContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-    backgroundColor: '#FFFFFF',
-    borderBottomWidth: 1,
-    borderBottomColor: '#DDDDDD',
-    borderTOPColor: 'gray',
-    borderTopWidth: 1,
-  },
-  filterButton: {
-    flex: 1,
-    paddingVertical: 12,
-    paddingHorizontal: 4,
-    borderRadius: 20,
-    backgroundColor: '#EEEEEE',
-    justifyContent: 'center',
-    marginRight: 2,
-    elevation: 5,
-
-
-  },
-  activeFilterButton: {
-    backgroundColor: '#007AFF',
-  },
-  filterButtonText: {
-
-    fontSize: 12,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#777777',
-
-
-  },
-  activeFilterButtonText: {
-    color: '#FFFFFF',
-  },
-  videoContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    paddingTop: 10,
-    paddingHorizontal: 10,
-
-  },
-  card: {
-    width: 150,
-    height: 200,
-    margin: 10,
-    borderRadius: 10,
-    backgroundColor: '#FFFFFF',
-    elevation: 1,
-
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-  },
-  overlay: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    padding: 10,
-    backgroundColor: 'rgba(0, 0, 0, 0.6)',
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-  },
-  title: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  description: {
-    color: '#FFFFFF',
-    fontSize: 12,
-  },
-
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-
-
-  },
-});
-
+/*
+<View style={styles.overlay}>
+                <Text style={styles.title}>{video.title}</Text>
+                <Text style={styles.description}>{video.description}</Text>
+              </View>*/
 export default VideoCourses;
---
+++ pages/VideoCoursesStyles.js
@@ -0,0 +1,104 @@
+const styles = {
+    container: {
+      flex: 1,
+      backgroundColor: '#F6F6F6',
+      resizeMode: 'cover',
+      //justifyContent: 'center',
+    },
+    filterContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 10,
+      backgroundColor: '#FFFFFF',
+      borderBottomWidth: 1,
+      borderBottomColor: '#DDDDDD',
+      borderTOPColor: 'gray',
+      borderTopWidth: 1,
+    },
+    filterButton: {
+      flex: 1,
+      paddingVertical: 12,
+      paddingHorizontal: 4,
+      borderRadius: 20,
+      backgroundColor: '#EEEEEE',
+      justifyContent: 'center',
+      marginRight: 2,
+      elevation: 5,
+  
+  
+    },
+    activeFilterButton: {
+      backgroundColor: '#007AFF',
+    },
+    filterButtonText: {
+  
+      fontSize: 12,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      color: '#777777',
+  
+  
+    },
+    activeFilterButtonText: {
+      color: '#FFFFFF',
+    },
+    videoContainer: {
+      
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingTop: 0,
+     
+  
+    },
+    card: {
+      width: '100%',
+      aspectRatio: 16 / 9,
+      marginBottom: 60,
+    
+    },
+    imageContainer:{
+        flex: 1,
+
+    },
+
+    image: {
+     
+      width: '100%',
+      height: 200,
+   
+      resizeMode: 'cover',
+    },
+    overlay: {
+
+
+      position: 'absolute',
+      bottom: -60,
+      left: 0,
+      right: 0,
+      padding: 10,
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+
+    },
+    title: {
+      color: '#e0e0e0',
+      fontSize: 16,
+      fontWeight: 'bold',
+      marginBottom: 5,
+    },
+    description: {
+      color: 'grey',
+      fontSize: 12,
+    },
+  
+    backgroundImage: {
+      flex: 1,
+      resizeMode: 'cover',
+      justifyContent: 'center',
+      
+  
+    },
+  };
+
+export default styles;
